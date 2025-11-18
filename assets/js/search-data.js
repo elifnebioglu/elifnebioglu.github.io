92530965b0dd459f007abadc4817961963af382d
elifnebioglu <78234356+elifnebioglu@users.noreply.github.com>
2025-11-18 14:47:24 +0000

    Deploying to gh-pages from @ elifnebioglu/elifnebioglu.github.io@1368e76d28d5f3ab3e95f02a013acfd207551a05 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -46,11 +46,11 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-projects",
+          title: 'Projects',
+          description: "take a look at what I&#39;ve got involved in",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
+              window.location.href = "/projects/";
             },},{id: "projects-project-2",
           title: 'project 2',
           description: "a project with a background image and giscus comments",
